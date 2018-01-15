9f24fface196cc17b47eaa6fe22100d8a366dfb8
David M. Miller <daxdoxsi@gmail.com>
2018-01-15 02:14:54 -0600

    Adding About/History

--- src/config/routes.php
+++ src/config/routes.php
@@ -6,6 +6,9 @@ switch ($_GET['q'])
         $block_content =  PATH_CONTROLLER.'lang_selector/lang.php';
         $load_template = false;
         break;
+    case 'about/history':
+        $block_content =  PATH_CONTROLLER.'about/history.php';
+        break;
     case '':
         $block_content =  PATH_CONTROLLER.'default.php';
         //$load_template = false;
---
+++ src/controllers/about/history.php
@@ -0,0 +1,3 @@
+<?php
+
+require PATH_VIEW.'about/history.php';
