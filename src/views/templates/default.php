84e085031b8d6ea3d169a3ef264968b429c8dc97
David M. Miller <daxdoxsi@gmail.com>
2018-01-15 23:32:02 -0600

    Adding Bootstrap and jQuery libraries

--- src/views/templates/default.php
+++ src/views/templates/default.php
@@ -6,10 +6,10 @@
     <title>Z506 Solutions</title>
 
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
 
     <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
@@ -27,10 +27,10 @@
 
     <!-- Latest compiled and minified JavaScript -->
     <script
-            src="https://code.jquery.com/jquery-3.2.1.min.js"
+            src="/js/jquery-3.2.1.min.js"
             integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
             crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="/js/bootstrap.min.js"></script>
     <script src="/js/scripts.js"></script>
 </body>
 </html>
