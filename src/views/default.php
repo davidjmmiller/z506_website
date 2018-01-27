251e1fe88bdd012b202371dcd26765ab73a5d666
David M. Miller <daxdoxsi@gmail.com>
2018-01-27 11:01:07 -0600

    Adding content to the main page

--- src/views/default.php
+++ src/views/default.php
@@ -41,31 +41,32 @@
 <div class="row home-info">
     <div class="col-md-4">
         <span class="center-block glyphicon glyphicon-road"></span>
-        <h2>One</h2><p>Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...</p>
+        <h2>Unlimited Traffic</h2>
+        <p>Your convenient flat : Transfer as much data as you want every month - over an extremely fast network connection and completely without extra costs.</p>
     </div>
     <div class="col-md-4">
         <span class="center-block glyphicon glyphicon-check"></span>
-        <h2>Two</h2>
-        <p>Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...</p>
+        <h2>Total Freedom</h2>
+        <p>Get started, stay on track - or even alter your plans. No minimum term. Monthly cancellation. Great flexibility.</p>
     </div>
     <div class="col-md-4">
         <span class="center-block glyphicon glyphicon-flash"></span>
-        <h2>Three</h2>
-        <p>Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...</p>
+        <h2>24/7 Support</h2>
+        <p>When you need support, we are there for you 24/7. You can find the best practical info in exchange with other Z506 customers on our forum.</p>
     </div>
     <div class="col-md-4">
         <span class="center-block glyphicon glyphicon-education"></span>
-        <h2>Four</h2>
-        <p>Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...</p>
+        <h2>Consulting</h2>
+        <p>You're not alone, we offer extensive trainings to our clients to start using our several products and also we have many options for develop your business</p>
     </div>
     <div class="col-md-4">
         <span class="center-block glyphicon glyphicon-grain"></span>
-        <h2>Five</h2>
-        <p>Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...</p>
+        <h2>No one is greener</h2>
+        <p>Being environmentally friendly: Our DataDock is one of the greenest and most secure data centers in USA - centrally located and with optimum connectivity.</p>
     </div>
     <div class="col-md-4">
         <span class="center-block glyphicon glyphicon-cloud-upload"></span>
-        <h2>Six</h2>
-        <p>Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...</p>
+        <h2>Capacity</h2>
+        <p>We growth with you, all our plans are scalable and flexible to ensure your business activity.</p>
     </div>
 </div>
